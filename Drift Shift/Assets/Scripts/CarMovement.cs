9d231dcc7da71e3ed2891b9461f727ef4775e2e5
Atharv Dhanaji Pol <118676468+Atharvp1001@users.noreply.github.com>
2025-08-09 23:08:27 +0530

    Added Test car and make it able to move around

---
+++ Drift Shift/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+
+    private Vector3 MoveForce;
+
+    [Header("Movement")]
+    [SerializeField]private float MoveSpeed = 50f;
+    [SerializeField]private float SteerAngle = 30f;
+
+    [Header("Drag")]
+    [SerializeField]private float Drag = 0.98f;
+    [SerializeField]private float MaxSpeed = 40f;
+
+    [Header("Traction")]
+    [SerializeField]private float Traction = 1;
+
+    // Update is called once per frame
+    void Update()
+    {   
+        //Moving
+        MoveForce += transform.forward * MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position += MoveForce * Time.deltaTime;
+
+        //Steering
+        float SteerInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * SteerInput * MoveForce.magnitude * SteerAngle * Time.deltaTime);
+
+        //Drag
+        MoveForce *= Drag;
+        MoveForce = Vector3.ClampMagnitude(MoveForce, MaxSpeed); //Add a limiter to speed
+
+        //Traction
+        MoveForce = Vector3.Lerp(MoveForce.normalized, transform.forward, Traction * Time.deltaTime) * MoveForce.magnitude;
+    }
+}

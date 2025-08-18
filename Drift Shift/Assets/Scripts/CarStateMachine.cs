7169e4a96dc436dfecff2ecc44f4f26b9009773b
Atharv Dhanaji Pol <118676468+Atharvp1001@users.noreply.github.com>
2025-08-19 01:14:47 +0530

    Complete the CarStateMachine

--- Drift Shift/Assets/Scripts/CarStateMachine.cs
+++ Drift Shift/Assets/Scripts/CarStateMachine.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public enum CarState //Health Related States for the car 
 {
     Normal,
-    Damaged,
     Invincible,
     Dead
 }
 public class CarStateMachine : MonoBehaviour
 {
+    public CarState currentState = CarState.Normal;
+    
     [Header("Health")]
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int currentHealth;
@@ -19,18 +20,77 @@ public class CarStateMachine : MonoBehaviour
 
     [Header("Get Components")]
     private CarMovement movement;
+    private Animator animator;
     private BoostStateMachine boost;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentHealth = maxHealth;
         movement = GetComponent<CarMovement>();
         boost = GetComponent<BoostStateMachine>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(currentState)
+        {
+            case CarState.Normal:
+                if (currentHealth < maxHealth * 0.25f) //To check if the helth is less than 25% 
+                {
+                    animator.Play("Damaged");
+                }
+                    
+                else
+                {
+                    animator.Play("Normal");
+                }
+                break; 
+
+            case CarState.Dead:
+                movement.enabled = false;
+                animator.Play("Dead");
+                break; 
+
+            case CarState.Invincible:
+                animator.Play("Invincible");
+                HandleInvincible();
+                break; 
+        }
     }
+
+    public void TakeDamage(int damage = 10)
+    {
+        if(currentState == CarState.Dead || currentState == CarState.Invincible)
+        {
+            return;
+        }
+
+        currentHealth -= damage; //Take Damage
+
+        if (currentHealth <=0)//Check if the player is dead 
+        {
+            currentHealth = 0;
+            currentState = CarState.Dead;
+        }
+        else //Start Invincible after taking dmg 
+        {
+            currentState = CarState.Invincible;
+            invincibleTimer = invincibleDuration;
+        }
+    }
+
+    public void HandleInvincible()
+    {
+        invincibleTimer -= Time.deltaTime; //Decrease Timer per Frame 
+
+        if (invincibleTimer <= 0)
+        {
+            currentState = CarState.Normal;
+        }
+    }
+
+
 }

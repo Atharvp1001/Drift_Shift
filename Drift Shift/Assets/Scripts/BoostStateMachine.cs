bb0689364dcb1ca2b3ca8223c8faa0d7c6a94a4a
Atharv Dhanaji Pol <118676468+Atharvp1001@users.noreply.github.com>
2025-08-27 17:28:35 +0530

    Just Some Minor Changes

--- Drift Shift/Assets/Scripts/BoostStateMachine.cs
+++ Drift Shift/Assets/Scripts/BoostStateMachine.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
+public enum BoostState
+{
+    SpeedBoost,
+    HealBoost,
+    InvincibleBoost
+}
+
+
 public class BoostStateMachine : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
